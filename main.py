98f196e28a83c5e620ad9f09d77923a9cc5aba84
poojaasri balakumar <poojaasribalakumar@gmail.com>
2024-05-15 14:06:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+class color:
+  pink = "\033[35m"
+  red = "\033[31m"
+  yellow = "\033[33m"
+  end =  "\033[0m"
+  
+  
+print(color.yellow + logo + color.end)
+
+
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+not_direction = ["encode", "decode"]
+not_i = ["yes","no"]
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text = end_text + char
+    
+    
+  print(color.pink + f"Here's the {cipher_direction}d result: {end_text} \n" + color.end) 
+continue1 = True
+while continue1:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  if direction not in not_direction:
+    print(f"{direction} invalid!")
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  i = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
+  if i not in not_i:
+    print(f"{i} is invalid!")
+    i = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
+  if i == "no":
+    continue1 = False
+    print("Goodbye!")
+
+
+
+
+ 
